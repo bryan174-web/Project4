560baba7e6ceb3d892982c7349c35f6ab13c8573
Bryan Sevilla <bsevilla799@insite.4cd.edu>
2025-12-08 19:57:42 -0800

    Bonus and App together

---
+++ js/script2.js
@@ -0,0 +1,87 @@
+
+/* Bryan Sevilla
+date:12/82025
+assignment: script2 for project 4. Bonus */
+
+
+/* Bryan Sevilla
+date:11/2/2025
+assignment: script for codepen challenge */
+
+// Character list. Each house has a name and a code.  I don't understand why it needs a code
+const houses = [
+  {
+    code: "ST",
+    name: "Pikachu"
+  },
+  {
+    code: "LA",
+    name: "Charizard"
+  },
+  {
+    code: "BA",
+    name: "Mew"
+  },
+  {
+    code: "TA",
+    name: "Squirtle"
+  }
+];
+
+// Return an array of characters belonging to a house
+const getCharacters = houseCode => {
+  switch (houseCode) {
+    case "ST":
+      return ["Classified as an Electric-type Pokemon, Pikachu is a large yellow mouse with a lightning bolt-shaped tail, and red sacs on its cheek which can generate large amounts of electricity."];
+    case "LA":
+      return ["Charizard is regarded as one of the most famous and popular Pokemon, and has received positive reception from fans and the media since its debut into the series. "];
+    case "BA":
+      return ["Mew is one of the many fictional species in the Pokémon franchise. It is a small, pink, Psychic-type Mythical Pokémon, which are incredibly rare and powerful Pokémon typically available only via special events."];
+    case "TA":
+      return ["Squirtle is a Water type Pokemon.  Squirtle is a reptilian. It has blue body with purple eyes.  It has a long tail that curls into a spiral."];
+    default:
+      return ["No house selected"]; 
+  }
+};
+
+// wait for the html to load
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+  //create reference to the select list
+  let house = document.getElementById('house');
+  
+ 
+  // loop through array and create an option tag for each item in the array.
+  houses.forEach( (item) => {
+    const option = document.createElement('OPTION');
+    option.value = item.code;
+    option.innerText = item.name;
+    house.appendChild(option);
+    // build the option and append it to the dropdown.
+
+  });
+
+   // display a list of the house members 
+  // when the select list has changed.
+  house.addEventListener('change', (e) => {
+    //get the house code from the select
+    const code = e.target.value;
+    let members = getCharacters(code);
+   
+    // DOM ref for ul to display members
+    const container = document.getElementById('characters');
+    
+    // clear out any previous member names
+    container.innerHTML = '';
+   
+    // create the li's and append to the ul
+    members.forEach((person)=> {
+      let item = document.createElement('LI');
+      item.innerText = person;
+      container.appendChild(item);
+    });
+    
+  }); //end addEventListener - change
+ 
+} //end init
